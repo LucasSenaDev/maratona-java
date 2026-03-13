db3f1acbecf904283710494f6965911c08a97a1a
LucasSenaDev <lucastsenna@gmail.com>
2026-03-13 20:40:06 -0300

    aula introdutória de Orientação a Objetos

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/domain/Student.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.domain;
+
+public class Student {
+    public String name;
+    public Integer age;
+    public Character sex;
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/StudentTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.domain.Student;
+
+public class StudentTest01 {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Lucas";
+        student.age = 21;
+        student.sex = 'M';
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.sex);
+
+    }
+}
