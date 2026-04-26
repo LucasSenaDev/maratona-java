8f17628e4f532f9dc09721e5010a58f6cae2e02f
LucasSenaDev <lucastsenna@gmail.com>
2026-04-25 21:31:20 -0300

    feature: exercício com associações

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/exercicio/domain/Local.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.exercicio.domain;
+
+public class Local {
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/exercicio/test/SeminariosTest01Exercicio.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.exercicio.test;
+
+public class SeminariosTest01Exercicio {
+}
