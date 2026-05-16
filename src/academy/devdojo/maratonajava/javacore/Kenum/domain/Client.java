472ee9661019ab3d47544a861fad24e9bef011ce
LucasSenaDev <lucastsenna@gmail.com>
2026-05-15 21:37:20 -0300

    feature: Aula 24 - Atribuindo valor a enumeração

--- src/academy/devdojo/maratonajava/javacore/Kenum/domain/Client.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/domain/Client.java
@@ -1,13 +1,18 @@
 package academy.devdojo.maratonajava.javacore.Kenum.domain;
 
 public class Client {
+    public enum TypePayment {
+        DEBITO,
+        CREDITO
+    }
     private String name;
     private TypeClient typeClient;
+    private TypePayment typePayment;
 
-    public Client(String name, TypeClient typeClient) {
+    public Client(String name, TypeClient typeClient, TypePayment typePayment) {
         this.name = name;
         this.typeClient = typeClient;
-
+        this.typePayment = typePayment;
     }
 
     @Override
@@ -15,23 +20,8 @@ public class Client {
         return "Client{" +
                 "name='" + name + '\'' +
                 ", typeClient=" + typeClient +
+                ", typeClientInt=" + typeClient.getValue() +
+                ", typePayment=" + typePayment +
                 '}';
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public TypeClient getTypeClient() {
-        return typeClient;
-    }
-
-    public void setTypeClient(TypeClient typeClient) {
-        this.typeClient = typeClient;
-    }
-
 }
--- src/academy/devdojo/maratonajava/javacore/Kenum/domain/TypeClient.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/domain/TypeClient.java
@@ -1,6 +1,15 @@
 package academy.devdojo.maratonajava.javacore.Kenum.domain;
 
 public enum TypeClient {
-    NATURAL_PERSON,
-    LEGAL_PERSON
+    NATURAL_PERSON(1),
+    LEGAL_PERSON(2);
+
+    private final int value;
+    TypeClient(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
