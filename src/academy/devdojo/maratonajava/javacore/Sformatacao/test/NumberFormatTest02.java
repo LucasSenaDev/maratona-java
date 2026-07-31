1fd04ce377c76898ef807d921cb72264d5d30fce
LucasSenaDev <lucastsenna@gmail.com>
2026-07-31 16:06:03 -0300

    feature: Aula 38 - Internacionalização de moeda com Locale

--- src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
@@ -4,30 +4,29 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-
         Locale LocaleBR = new Locale("pt", "BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
         NumberFormat[] nfa = new NumberFormat[4];
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeJP);
-        nfa[2] = NumberFormat.getInstance(LocaleBR);
-        nfa[3] = NumberFormat.getInstance(localeIT);
-        double value = 1_000.2130;
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(LocaleBR);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
+        double value = 1000.2130;
         for (NumberFormat numberFormat : nfa) {
-            numberFormat.setMaximumFractionDigits(2);
             //System.out.println(numberFormat.getMaximumFractionDigits());
             System.out.println(numberFormat.format(value));
         }
-        String stringValue = "1000.2130";
+
+        String stringValue = "￥1,000";
         try {
-            System.out.println(nfa[0].parse(stringValue));
+            System.out.println(nfa[1].parse(stringValue));
         } catch (ParseException e) {
             e.printStackTrace();
-          // throw new RuntimeException(e);
+            // throw new RuntimeException(e);
         }
+
     }
 }
